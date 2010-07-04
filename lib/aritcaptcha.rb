7d5a00f7fd18d209ab626d475e2913432d33fd17
Guilherme Nascimento <guilherme.ruby@gmail.com>
2010-07-04 12:14:42 -0300

    changing files

--- lib/aritcaptcha.rb
+++ lib/aritcaptcha.rb
@@ -1,4 +1,4 @@
 require 'aritcaptcha/aritcaptcha_helper'
 
 ActionView::Base.send(:include, Aritcaptcha::AritcaptchaHelper)
-# ActionController::Base.send(:include, Mathcha::MathchaVerify)
+ActionController::Base.send(:include, Aritcaptcha::AritcaptchaVerify)
--- lib/aritcaptcha/aritcaptcha_helper.rb
+++ lib/aritcaptcha/aritcaptcha_helper.rb
@@ -9,18 +9,22 @@ module Aritcaptcha
     OPERATIONS = [ADD, SUB, DIV, MUL]
 
     def aritcaptcha_tag(options={})
+      equation_key = Time.now.to_i 
       number1 = options[:one] || 50
       number2 = options[:two] || 50
       operation = options[:op] || OPERATIONS[rand(OPERATIONS.size)]
 
       equation, result = Aritcaptcha::Calculation.generate_calculation number1, number2, operation
-      
-      html_options = options[:html].inject([]){|dump, pair| dump << "#{pair[0]}=\"#{pair[1]}\""}
-      html_options.join(" ")
-      puts html_options
-       
+
+      session[:equation] = [equation_key, eval(equation)]
+
+      if options[:html]
+        html_options = options[:html].inject([]){|dump, pair| dump << "#{pair[0]}=\"#{pair[1]}\""}
+        html_options.join(" ")
+      end 
 
       html = "#{equation} = <input type='text' name='equation' #{html_options} />"
+      html << %{<input type="hidden" name="equation_key" value="#{equation_key}" />\n}
       html
     end
   end
